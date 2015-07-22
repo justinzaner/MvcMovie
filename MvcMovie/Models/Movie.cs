e05fcee5b170becf0d9c0776f60b87391fd188e3
justinzaner <justin.zaner@gmail.com>
2015-07-22 14:14:53 -0400

    Added ratings to movies and migrated the database
    Added "Ratings" property

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -16,10 +16,10 @@ namespace MvcMovie.Models
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 
     public class MovieDBContext : DbContext

896b5c2f6e6f183aba679ecab1b69e4c4f5c5fb9
justinzaner <justin.zaner@gmail.com>
2015-07-27 14:58:41 -0400

    Migrations and seeding
    Migrations

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -10,15 +10,29 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        // Added validation statement
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
         // Changed the release date format so that  it no longer includes a time of day, and the date will be formatted correctly.
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"),DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        // Added validation statement
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        // Added validation statement
+        [Range(1, 100),DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        // Added validation statement
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
